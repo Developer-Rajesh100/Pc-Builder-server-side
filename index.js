3e3cb0257b1f6e68d2e85b844272f0c480ce87f2
Developer-Rajesh <rajeshhabra100@gmail.com>
2022-05-25 18:05:00 +0530

    Github Code Push Problem

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.POST || 5000;
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fhk5k.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+//ALL API
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("pcBuilder").collection("product");
+
+        app.get("/product", async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        app.get("/product/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
+    } finally {
+    }
+}
+run().catch(console.dir);
+
+//FOOTER
+
+app.get("/", (req, res) => {
+    res.send("Server is Running");
+});
+
+app.listen(port, () => {
+    console.log("Listening to Port", port);
+});

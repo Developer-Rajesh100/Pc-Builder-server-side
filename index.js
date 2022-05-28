09c7eca193ab30c5a9affd6736da06b74af559a0
Developer-Rajesh <rajeshhabra100@gmail.com>
2022-05-28 16:30:07 +0530

    JWT token api done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -22,6 +23,8 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db("pcBuilder").collection("product");
+        const userCollection = client.db("pcBuilder").collection("user");
+        // const orderCollection = client.db("pcBuilder").collection("order");
 
         //FETCH ALL PRODUCT
 
@@ -30,6 +33,30 @@ async function run() {
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
+        });
+
+        //USER
+
+        app.put("/user/:email", async (req, res) => {
+            const email = req.params.email;
+            console.log(email);
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(
+                filter,
+                updateDoc,
+                options
+            );
+            const token = jwt.sign(
+                { email: email },
+                process.env.ACCESS_TOKEN_SECRET,
+                { expiresIn: "1h" }
+            );
+            res.send({ result, token });
         });
 
         //FETCH SINGLE PRODUCT
@@ -58,6 +85,14 @@ async function run() {
             const product = await productCollection.deleteOne(query);
             res.send(product);
         });
+
+        //ORDER DATA
+
+        // app.post("/order", async (req, res) => {
+        //     const order = req.body;
+        //     const result = await orderCollection.insertOne(order);
+        //     res.send(result);
+        // });
     } finally {
     }
 }

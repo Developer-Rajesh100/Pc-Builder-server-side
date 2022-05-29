0e43fb64dba3209207de0d6314799f48b45875ab
Developer-Rajesh <rajeshhabra100@gmail.com>
2022-05-29 10:40:37 +0530

    port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
-const port = process.env.POST || 5000;
+const port = process.env.PORT || 5000;
 
 // MIDDLEWARE
 app.use(cors());
